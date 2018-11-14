77ba0a39e683a26d0854be40dc5cf819ccd164c1
haridassvenkatesh <haridassvenkatesh@gmail.com>
2018-11-14 14:30:47 +0530

    first commit

---
+++ storage/framework/views/732b1dd0a8d4adfecaa39e6a1df7b95ad7383caf.php
@@ -0,0 +1,20 @@
+<!--
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body class="theme-red">
+-->
+    <?php echo $__env->make('templates.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('templates.top_menu', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('templates.side_menu', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+     <?php echo $__env->yieldContent('content'); ?>
+    <?php echo $__env->make('templates.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+<!--
+</body>
+
+</html>
+-->
